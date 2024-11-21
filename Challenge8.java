b82beb37a7a540d6afd3023357405f5ec4e6a1c7
Dhruti Purushotham <167422271+Dhru3@users.noreply.github.com>
2024-11-21 23:18:13 +0530

    Add files via upload

---
+++ Challenge8.java
@@ -0,0 +1,12 @@
+//Write a program Given an integer input, print the Absolute value of the input number.
+
+import java.util.*;
+
+public class Challenge8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(Math.abs(a));
+        }
+    }
